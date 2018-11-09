6a63dc493d995525443e8d72cb150b255f3ded50
RomainDE <romain@popschool.fr>
2018-11-09 21:44:27 +0100

    Java relation done

---
+++ src/main/java/com/lacle/backend/model/Lesson.java
@@ -0,0 +1,45 @@
+package com.lacle.backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@Entity
+@Table(name = "lessons")
+@Getter
+@Setter
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private Instant startDate;
+
+    @NotNull
+    private Instant endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private Teacher teacher;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    public Lesson() {
+    }
+
+    public Lesson(@NotNull Instant startDate, @NotNull Instant endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
