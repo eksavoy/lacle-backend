44ea666f41e2493aa91f43d8c075d28c3ed19962
RomainDE <romain@popschool.fr>
2018-11-09 22:27:12 +0100

    Add lesson search

--- src/main/java/com/lacle/backend/controller/LessonController.java
+++ src/main/java/com/lacle/backend/controller/LessonController.java
@@ -3,9 +3,13 @@ package com.lacle.backend.controller;
 import com.lacle.backend.model.Lesson;
 import com.lacle.backend.service.lesson.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -23,5 +27,10 @@ public class LessonController implements DefaultController<Lesson> {
     @Override
     public Lesson get(Long id) {
         return service.get(id);
+    }
+
+    @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Lesson> search(@RequestParam("start")Instant start, @RequestParam("end") Instant end){
+        return service.findAllBetweenDate(start, end);
     }
 }
